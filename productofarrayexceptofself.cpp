4527814c138d299cf415396eff305d6a675d6309
PRANJALI SHUKLA <shuklapranjali29@gmail.com>
2026-02-08 00:43:06 +0530

    added one leetcode question - product of array except self

---
+++ productofarrayexceptofself.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+
+        vector<int>pp(n,1);
+        vector<int>sp(n,1);
+        for(int i=1;i<n;i++){
+            pp[i]=pp[i-1]*nums[i-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            sp[i]=sp[i+1]*nums[i+1];
+        }
+        vector<int>ans(n,1);
+        for(int i=0;i<n;i++){
+            ans[i]=pp[i]*sp[i];
+        }
+        return ans;
+        
+    }
+};
+//uske badd ctrl+s krke save krna
