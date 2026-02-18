ac677124514eb0866a448e1e89487bab1eabfd04
PRANJALI SHUKLA <shuklapranjali29@gmail.com>
2026-02-18 18:34:55 +0530

    added  llcycle1 solution using slow and fast poinnter

---
+++ llcycle1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        bool ans=true;
+        if(head==NULL){
+            return false;//base case
+        }
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+
+        
+    }
+};
