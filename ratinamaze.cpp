7fd074261956932ad2e285f5673d1716db259355
PRANJALI SHUKLA <shuklapranjali29@gmail.com>
2026-03-25 00:04:45 +0530

    added new solution

--- ratinamaze.cpp
+++ ratinamaze.cpp
@@ -1,48 +1,58 @@
 class Solution {
   public:
-  void sub(int i,int j,int n,vector<vector<int>>&visited,vector<vector<int>>&maze,string move,vector<string>&ans){
+  void rat(int i,int j,vector<vector<int>>&visited,vector<vector<int>>&maze,int n,string s,vector<string>&ans){
       if(i==n-1 && j==n-1){
-          ans.push_back(move);
+          ans.push_back(s);
           return;
-      }
-      //down
-      if(i+1<n && !visited[i+1][j] && maze[i+1][j]==1){
-          visited[i][j]=1;
-          sub(i+1,j,n,visited,maze,move+"D",ans);
-          visited[i][j]=0;
           
       }
-      //left
+      if(i+1<n && !visited[i+1][j] && maze[i+1][j]==1){
+          visited[i+1][j]=1;
+          s.push_back('D');
+          rat(i+1,j,visited,maze,n,s,ans);
+          s.pop_back();
+          visited[i+1][j]=0;
+         
+          
+      }
       if(j-1>=0 && !visited[i][j-1] && maze[i][j-1]==1){
-          visited[i][j]=1;
-          sub(i,j-1,n,visited,maze,move+"L",ans);
-          visited[i][j]=0;
+          visited[i][j-1]=1;
+          s.push_back('L');
+          rat(i,j-1,visited,maze,n,s,ans);
+          s.pop_back();
+          visited[i][j-1]=0;
       }
-      //right
-      if(j+1<n && !visited[i][j+1] && maze[i][j+1]==1){
-          visited[i][j]=1;
-          sub(i,j+1,n,visited,maze,move+"R",ans);
-          visited[i][j]=0;
-      }
-      //up
-      if(i-1>=0 && !visited[i-1][j] && maze[i-1][j]==1){
-          visited[i][j]=1;
-          sub(i-1,j,n,visited,maze,move+"U",ans);
-          visited[i][j]=0;
-      }
+       if(j+1<n && !visited[i][j+1] && maze[i][j+1]==1){
+          visited[i][j+1]=1;
+          s.push_back('R');
+          rat(i,j+1,visited,maze,n,s,ans);
+          s.pop_back();
+          visited[i][j+1]=0;
+       }
+       if(i-1>=0 && !visited[i-1][j] && maze[i-1][j]==1){
+          visited[i-1][j]=1;
+          s.push_back('U');
+          rat(i-1,j,visited,maze,n,s,ans);
+          s.pop_back();
+          visited[i-1][j]=0;
+       }
+                
+      
+      
+      
+      
+      
   }
     vector<string> ratInMaze(vector<vector<int>>& maze) {
         int n=maze.size();
-        vector<vector<int>>visited(n,vector<int>(n,0));
-        string move="";
         vector<string>ans;
-       
-        sub(0,0,n,visited,maze,"",ans);
+        vector<vector<int>>visited(n,vector<int>(n,0));
+        if(maze[0][0]==0){
+            return ans;
+        }
+        visited[0][0]=1;
+        rat(0,0,visited,maze,n,"",ans);
         return ans;
-        
-       
-        
-        
         
     }
 };
