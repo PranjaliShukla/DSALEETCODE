671d2e7469cbf21a25df59ce3f4698f95605cdfd
PRANJALI SHUKLA <shuklapranjali29@gmail.com>
2026-04-10 23:44:19 +0530

    added

---
+++ 151.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int j=s.size()-1;
+        int i=0;
+        string si;
+        string r;
+        for(int i=j;i>=0;i--){
+            
+            if(s[i]!=' '){
+                r=r+s[i];
+            }
+            else{
+                if(!r.empty()){
+                reverse(r.begin(),r.end());
+                if(!si.empty())
+                    si+=" ";
+                    si=si+r;
+                    r="";
+                
+                }
+                
+            }
+            
+        }
+        if(!r.empty()){
+            reverse(r.begin(),r.end());
+            if(!si.empty())
+            si+=" ";
+            si=si+r;
+        }
+        return si;
+        
+        
+    }
+};
