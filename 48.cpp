102e1330eeacaffa2a2a491f3e90df0998151b67
PRANJALI SHUKLA <shuklapranjali29@gmail.com>
2026-04-22 18:11:00 +0530

    revised

--- 48.cpp
+++ 48.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int s1=matrix.size();
-        for(int i=0;i<s1-1;i++){//till second last element
-          
+        for(int i=0;i<s1-1;i++){
             for(int j=i+1;j<s1;j++){
                 swap(matrix[i][j],matrix[j][i]);
-            
-    
-
             }
         }
         for(int i=0;i<s1;i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
-        
         
     }
 };
