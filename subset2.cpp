0f7e785d043a66cf7d757dd2c21a014f029f8e0b
PRANJALI SHUKLA <shuklapranjali29@gmail.com>
2026-05-17 22:15:25 +0530

    revised

--- subset2.cpp
+++ subset2.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-     void subset(int i,int n,vector<int>&temp,set<vector<int>>&ans,vector<int>&nums){
-        if(i==n){
-           
-            ans.insert(temp);
-            return;
-        }
-        temp.push_back(nums[i]);
-        subset(i+1,n,temp,ans,nums);
-        temp.pop_back();
-        subset(i+1,n,temp,ans,nums);
-     }
+void subset(int i,int n,vector<int>&temp,vector<int>&nums,set<vector<int>>&ans){
+      if(i==n){
+        ans.insert(temp);
+        return;
+      }
+      temp.push_back(nums[i]);
+      subset(i+1,n,temp,nums,ans);
+     temp.pop_back();
+     subset(i+1,n,temp,nums,ans);
+}
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
         vector<int>temp;
         set<vector<int>>ans;
-        subset(0,n,temp,ans,nums);
+        subset(0,n,temp,nums,ans);
         return vector<vector<int>>(ans.begin(),ans.end());
         
     }
