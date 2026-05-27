3940b66a3dc3c5fe11636a4863178cea12819f27
PRANJALI SHUKLA <shuklapranjali29@gmail.com>
2026-05-28 02:05:38 +0530

    added sql question

---
+++ 184.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select d.name as Department, e.name as Employee, e.salary as Salary from Employee as e join Department as d on e.departmentId=d.id where (e.departmentId,e.salary) IN (select e.departmentId,max(salary) from Employee as e group by e.departmentId);
