e9ebafc17dca43f738012cae84c992d426e0121e
PRANJALI SHUKLA <shuklapranjali29@gmail.com>
2026-05-28 14:08:28 +0530

    added sql query

---
+++ 1141.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select a.activity_date as day,COUNT(DISTINCT a.user_id) as active_users from Activity as a where a.activity_date>'2019-06-27' AND a.activity_date<='2019-07-27' group by a.activity_date;
